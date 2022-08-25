f611a16da3f96d8ba3873f1a5c6bf8d8f0993ff3
Ryan Clarke <rclarke@rclarke.ca>
2022-08-24 21:02:17 -0700

    added __convert_port_scan_target_string_to_list

--- scan.py
+++ scan.py
@@ -1,4 +1,5 @@
 import ipaddress
+import string
 from enum import Enum
 from typing import List, Any
 
@@ -27,15 +28,25 @@ class Scan():
         if scan_method == ScanMethod.single:
             targets.append(ipaddress.ip_address(scan_target))
         elif scan_method == ScanMethod.cidr:
+            # TODO implement CIDR conversion
             pass
         elif scan_method == ScanMethod.range:
+            # TODO implement range conversion
             pass
         elif scan_method == ScanMethod.domains:
+            # TODO implement domains conversion
             pass
         return targets
 
-    def __init__(self, scan_method: ScanMethod, scan_target: str, port_scan_method: PortScanMethod, port_scan_target: str):
+    def __convert_port_scan_target_string_to_list(self, port_scan_target):
+        try:
+            target = [int(port) for port in port_scan_target.split(',')]
+        except:
+            raise ValueError("Non-integer in list of ports")
+
+    def __init__(self, scan_method: ScanMethod, scan_target: str, port_scan_method: PortScanMethod,
+                 port_scan_target: str):
         self.scan_method = scan_method
         self.port_scan_method = port_scan_method
         self.scan_target = self.__convert_scan_target_string_to_list(scan_target, scan_method)
-        self.port_scan_target = port_scan_target
+        self.port_scan_target = self.__convert_port_scan_target_string_to_list(port_scan_target)
