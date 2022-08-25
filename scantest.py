e1b49cbd666ab005f526081392eceedfb8c8c270
Ryan Campbell-Clarke <ryan.campbell-clarke@yukon.ca>
2022-08-25 11:54:25 -0700

    got items I want from cert in scantest.py

--- scantest.py
+++ scantest.py
@@ -4,6 +4,7 @@ import ssl
 from cryptography.x509.oid import ExtensionOID
 import sys
 
+#target = "yukon.ca"
 target = "expired.badssl.com"
 port = 443
 # create default context
@@ -17,8 +18,18 @@ with socket.create_connection((target, port)) as sock:
         der_cert = wrapped_sock.getpeercert(True)
         cert = x509.load_der_x509_certificate(der_cert)
         # show cert expiry date
-        #print(cert.extensions)
-        san = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+        #print(cert)
+
         # Get SANs
+
+        subject = cert.subject.rfc4514_string()
+        issuer=cert.issuer.rfc4514_string()
+        not_valid_after=cert.not_valid_after
+        not_valid_before=cert.not_valid_before
+        serial_number=cert.serial_number
+        signature_hash_algorithm=cert.signature_hash_algorithm.name
+        version=cert.version.name
+        subject_alternative_names = []
+        san = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
         for san in san.value:
-            print(san.value)
+            subject_alternative_names.append(san.value)
