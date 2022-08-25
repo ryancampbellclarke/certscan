6e8e5e67f9b8d6a4021d5055a3d63798c6777125
Ryan Campbell-Clarke <ryan.campbell-clarke@yukon.ca>
2022-08-25 14:54:55 -0700

    updated py2exe setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['main.py'])
+setup(console = [
+    {
+        "script": "main.py",
+        #"icon_resources": [(0, "favicon.ico")], ### Icon to embed into the PE file.
+        "dest_base" : "certscan"
+    }],
+    data_files=[("dist",["config.ini",])]
+)
